f22a60353623e04275184d9bded8156dc03f7a94
FzFStormZ <dylaa.degail@gmail.com>
2020-12-19 13:58:36 +0100

    Add seasons on show page + begin episodes

--- src/Controller/SeriesController.php
+++ src/Controller/SeriesController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Episode;
+use App\Entity\Season;
 use App\Entity\Series;
 use App\Form\SeriesType;
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -33,12 +36,86 @@ class SeriesController extends AbstractController
      */
     public function show(Series $series): Response
     {
+        // To get the poster
         $stream = $series->getPoster();
         $poster = base64_encode(stream_get_contents($stream));
+
+        // To get seasons
+        $season = $this->getDoctrine()
+            ->getRepository(Season::class);
+
+        $seasons = $season->findBy(['series' => $series->getId()]); // Get seasons about the serie instance
+
+        // To get episodes
+        $episodes = $this->getDoctrine()
+            ->getRepository(Episode::class)
+            ->findBy(['season' => $season_id]); // A REVOIR !!
         
         return $this->render('series/show.html.twig', [
             'series' => $series,
-            'poster' => $poster
+            'poster' => $poster,
+            'seasons' => $seasons,
+            'episodes' => $episodes
         ]);
     }
+
+
+    /**
+     * @Route("/new", name="series_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $series = new Series();
+        $form = $this->createForm(SeriesType::class, $series);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($series);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('series_index');
+        }
+
+        return $this->render('series/new.html.twig', [
+            'series' => $series,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="series_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Series $series): Response
+    {
+        $form = $this->createForm(SeriesType::class, $series);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('series_index');
+        }
+
+        return $this->render('series/edit.html.twig', [
+            'series' => $series,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="series_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Series $series): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$series->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($series);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('series_index');
+    }
+
+
 }
