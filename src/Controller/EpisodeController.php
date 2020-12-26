cd6587641a52bbcc80fafd1c3c3ddca95c63b701
Julien Chabrier <julien.chabrier@etu.u-bordeaux.fr>
2020-12-26 14:22:29 +0100

    episode show

--- src/Controller/EpisodeController.php
+++ src/Controller/EpisodeController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Season;
+use App\Entity\Series;
+use App\Entity\Episode;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +13,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class EpisodeController extends AbstractController
 {
     /**
-     * @Route("/episode", name="episode")
+     * @Route("/series/{serie_id}/seasons/{season_id}/episode/{ep_id}", name="episode_show", methods={"GET", "POST"}, requirements={"series_id":"\d+", "seasons_id":"\d+"})
      */
-    public function index(): Response
+    public function index(Series $serie_id, Season $season_id, Episode $ep_id, Request $request): Response
     {
+
+
         return $this->render('episode/index.html.twig', [
-            'controller_name' => 'EpisodeController',
+            'serie' => $serie_id,
+            'season' => $season_id,
+            'episode' => $ep_id,
         ]);
     }
 }
--- src/Controller/SeasonController.php
+++ src/Controller/SeasonController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use App\Entity\Season;
 use App\Entity\Series;
 use App\Entity\Episode;
